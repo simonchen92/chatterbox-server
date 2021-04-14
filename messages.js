238c4ea3474caa3eae7e49306ccee7267b63f19b
simonchen92 <simon.k.chen92@gmail.com>
2021-04-14 15:53:58 -0400

    updating npm packages and deleting moment

--- messages.js
+++ messages.js
@@ -1,12 +1,8 @@
-'use strict'
-const moment = require('moment')
-
 function formatMessage(user, text) {
   return {
     user,
     text,
-    time: moment().format('h:mm A')
   };
 }
 
-module.exports = formatMessage
+module.exports = formatMessage;
